8e0e0708d3a63f3fd4d072616f7dc92c64b9243c
StoneInHisWorld <1195290789@qq.com>
2023-11-22 17:23:19 +0800

    fix SSIM compute issue.

--- networks/layers/common_layers.py
+++ networks/layers/common_layers.py
@@ -3,18 +3,18 @@ from torch import nn
 
 
 def SSIM(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    mean_x, mean_y = torch.mean(y_hat, dim=0), torch.mean(y, dim=0)
-    var_x, var_y = torch.var(y_hat), torch.var(y)
+    mean_x, mean_y = [torch.mean(t, dim=list(range(1, len(t.shape))), keepdim=True) for t in [y_hat, y]]
+    var_x, var_y = [torch.var(t, dim=list(range(1, len(t.shape))), keepdim=True) for t in [y_hat, y]]
     conv_xy = torch.mean((y_hat - mean_x) * (y - mean_y))
     R = torch.tensor(255)
     c1, c2 = torch.sqrt(R * 0.01), torch.sqrt(R * 0.03)
     numerator = (2 * mean_x * mean_y + c1) * (2 * conv_xy + c2)
     denominator = (mean_x ** 2 * mean_y ** 2 + c1) * (var_x ** 2 * var_y ** 2 + c2)
-    ssim = numerator / denominator
-    return torch.mean(ssim)
+    return numerator / denominator
 
 
 class SSIMLoss(nn.Module):
 
     def forward(self, y_hat, y):
-        return 1 - SSIM(y_hat, y)
+        ssim_of_each_sample = SSIM(y_hat, y)
+        return torch.mean(1 - ssim_of_each_sample)
