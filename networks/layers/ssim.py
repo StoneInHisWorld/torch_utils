cbbf3e7fd4fa17022c11224aa9958a5de0b001a1
StoneInHisWorld <1195290789@qq.com>
2023-12-14 13:33:57 +0800

    new layer for SSIM calculation & SSIM Loss calculation.

--- networks/layers/ssim.py
+++ networks/layers/ssim.py
@@ -2,10 +2,11 @@ import warnings
 
 import torch
 from torch import nn
+import utils.tools as tools
+import torchvision.transforms as T
 
 
-def SSIM(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    # TODO：conv_xy大得吓人！
+def calculate_ssim(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     mean_x, mean_y = [torch.mean(t, dim=list(range(1, len(t.shape))), keepdim=True) for t in [y_hat, y]]
     std_x, std_y = [torch.std(t, dim=list(range(1, len(t.shape))), keepdim=True) for t in [y_hat, y]]
     conv_xy = torch.sum((y_hat - mean_x) * (y - mean_y), dim=list(range(1, len(y.shape))), keepdim=True) / (len(y_hat) - 1)
@@ -18,17 +19,51 @@ def SSIM(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 
 class SSIMLoss(nn.Module):
 
-    def __init__(self, mute=True):
+    def __init__(self, mode: str = 'L', mute=True):
         """
-        SSIM损失层。计算每对y_hat与y的图片结构相似度，并求其平均逆作为损失值。
+        SSIM损失层。计算每对y_hat与y的图片结构相似度，并求其平均逆（1 - mean(ssim)）作为损失值。
         计算公式为：ls =
         """
         self.mute = mute
+        self.mode = mode
         super().__init__()
 
     def forward(self, y_hat, y):
-        ssim_of_each_sample = SSIM(y_hat, y)
-        for ssim in ssim_of_each_sample:
-            if ssim < 0 and not self.mute:
-                warnings.warn(f'出现了负值SSIM={ssim}！')
-        return torch.mean(1 - ssim_of_each_sample)
+        # 将y_hat, y反归一化
+        # TODO: Untested!
+        y_hat, y = [tools.tensor_to_img(t, self.mode) for t in [y_hat, y]]
+        transformer = T.PILToTensor()
+        y_hat, y = [transformer(t) for t in [y_hat, y]]
+        # 计算SSIM
+        ssim_of_each = calculate_ssim(y_hat, y)
+        if not self.mute:
+            for ssim in ssim_of_each:
+                if ssim < 0:
+                    warnings.warn(f'出现了负值SSIM={ssim}！')
+        return 1 - torch.mean(ssim_of_each, dim=list(range(1, len(ssim_of_each.shape))))
+
+
+class SSIM(nn.Module):
+
+    def __init__(self, mode: str = 'L', mute=True):
+        """
+        SSIM计算层。计算批次中，每对y_hat与y的图片结构相似度，并求其平均作为损失值。
+        计算公式为：ls =
+        """
+        self.mute = mute
+        self.mode = mode
+        super().__init__()
+
+    def forward(self, y_hat, y):
+        # 将y_hat, y反归一化
+        # TODO: Untested!
+        y_hat, y = [tools.tensor_to_img(t, self.mode) for t in [y_hat, y]]
+        transformer = T.PILToTensor()
+        y_hat, y = [transformer(t) for t in [y_hat, y]]
+        # 计算SSIM
+        ssim_of_each = calculate_ssim(y_hat, y)
+        if not self.mute:
+            for ssim in ssim_of_each:
+                if ssim < 0:
+                    warnings.warn(f'出现了负值SSIM={ssim}！')
+        return torch.mean(ssim_of_each, dim=list(range(1, len(ssim_of_each.shape))))
