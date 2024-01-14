9315d3d8c75efc3c9cefdd79930da939f6c4ee12
StoneInHisWorld <1195290789@qq.com>
2024-01-14 21:11:30 +0800

    doc

--- data_related/data_related.py
+++ data_related/data_related.py
@@ -163,7 +163,18 @@ def data_slicer(data_portion=1., shuffle=True, *args: Sized):
 
 
 def normalize(data: torch.Tensor, epsilon=1e-5) -> torch.Tensor:
+    """
+    进行数据标准化。
+    :param data: 需要进行标准化的数据。
+    :param epsilon: 防止分母为0的无穷小量。
+    :return: 标准化的数据
+    """
     mean, std = [func(data, dim=list(range(2, len(data.shape))), keepdim=True)
                  for func in [torch.mean, torch.std]]
-    computer = transforms.Normalize(mean, std + epsilon)
-    return computer(data)
+    if len(data.shape) == 4:
+        computer = transforms.Normalize(mean, std + epsilon)
+        return computer(data)
+    elif len(data.shape) == 1:
+        return (data - mean) / (std + epsilon)
+    else:
+        raise Exception(f'不支持的数据形状{data.shape}！')
