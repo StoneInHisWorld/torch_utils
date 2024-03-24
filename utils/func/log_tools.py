e508b679bcb70a8857e836126da17be39fa93de9
StoneInHisWorld <1195290789@qq.com>
2024-03-24 12:28:34 +0800

    delete unused codes.

--- utils/func/log_tools.py
+++ utils/func/log_tools.py
@@ -45,20 +45,18 @@ def write_log(path: str, **kwargs):
                 time.sleep(1)
 
 
-# def plot_history(history, mute=False, title=None,
-#                  ls_ylabel=None, acc_ylabel=None, lr_ylabel=None,
-#                  savefig_as=None, accumulative=False):
 def plot_history(history,
                  mute=False, title=None,
                  savefig_as=None, accumulative=False,
                  max_nrows=3, figsize=(7, 7.5)):
-    """
-    绘制训练历史变化趋势图
+    """绘制训练历史变化趋势图
     :param history: 训练历史数据
     :param mute: 绘制完毕后是否立即展示成果图
     :param title: 绘制图标题
     :param savefig_as: 保存图片路径
     :param accumulative: 是否将所有趋势图叠加在一起
+    :param figsize: 图片大小
+    :param max_nrows: 最大子图行数
     :return: None
     """
     # 获取子图标题以计算趋势图行列数
@@ -68,7 +66,6 @@ def plot_history(history,
     ]))
     n_cols = int(np.ceil(len(subplots_titles) / max_nrows))
     fig, axes = plt.subplots(
-        # len(subplots_titles), 1, sharex='col', figsize=(7, 7.5)
         min(max_nrows, len(subplots_titles)), n_cols,
         sharex='col', figsize=(7, 7.5)
     )
@@ -109,45 +106,6 @@ def plot_history(history,
     if not accumulative:
         plt.close(fig)
         plt.clf()
-
-    # fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', figsize=(7, 7.5))
-    # ax1.set_title('LOSS')
-    # ax2.set_title('ACCURACY')
-    # ax3.set_title('LEARNING_RATE')
-    # ax3.set_xlabel('epochs')
-    # for label, log in history:
-    #     if label.find('_l') != -1:
-    #         # 绘制损失值history
-    #         ax1.plot(range(1, len(log) + 1), log, label=label)
-    #     elif label.find('_acc') != -1:
-    #         # 绘制准确率history
-    #         ax2.plot(range(1, len(log) + 1), log, label=label)
-    #     elif label.find('lr') != -1:
-    #         # 绘制学习率history
-    #         for i in range(len(log[0])):
-    #             lr_of_cur_group = [epoch_lrs[i] for epoch_lrs in log]
-    #             ax3.plot(range(1, len(log) + 1), lr_of_cur_group, label=f'lr_{i}')
-    # if ls_ylabel:
-    #     ax1.set_ylabel(ls_ylabel)
-    # if acc_ylabel:
-    #     ax2.set_ylabel(acc_ylabel)
-    # if lr_ylabel:
-    #     ax3.set_ylabel(lr_ylabel)
-    # if title:
-    #     fig.suptitle(title)
-    # ax1.legend()
-    # ax2.legend()
-    # ax3.legend()
-    # if savefig_as:
-    #     if not os.path.exists(os.path.split(savefig_as)[0]):
-    #         os.makedirs(os.path.split(savefig_as)[0])
-    #     plt.savefig(savefig_as)
-    #     print('已保存历史趋势图')
-    # if not mute:
-    #     plt.show()
-    # if not accumulative:
-    #     plt.close(fig)
-    #     plt.clf()
 
 
 def get_logData(log_path, exp_no) -> dict:
