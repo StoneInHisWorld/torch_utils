b3c576c70a3316fcefb7aec890388c976eeef1d3
StoneInHisWorld <1195290789@qq.com>
2024-03-24 21:47:34 +0800

    add PCC

--- data_related/criteria.py
+++ data_related/criteria.py
@@ -1,6 +1,9 @@
 import torch
 from skimage.metrics import (structural_similarity as ssim,
                              peak_signal_noise_ratio as psnr)
+from scipy.stats import pearsonr
+
+from data_related.data_related import normalize
 
 
 def ARGMAX(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged=True) -> float:
@@ -27,6 +30,16 @@ def PSNR(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
     y = Y.cpu().numpy().squeeze()
     y_hat = Y_HAT.cpu().numpy().squeeze()
     result = torch.tensor([psnr(i, j, data_range=1) for i, j in zip(y, y_hat)])
+    if size_averaged:
+        return torch.sum(result)
+    else:
+        return result
+
+
+def PCC(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
+    y = normalize(Y).cpu().numpy()
+    y_hat = normalize(Y_HAT).cpu().numpy()
+    result = torch.tensor([pearsonr(i.flatten(), j.flatten())[0] for i, j in zip(y, y_hat)])
     if size_averaged:
         return torch.sum(result)
     else:
