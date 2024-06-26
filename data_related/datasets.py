be04ee80f52f11ac8e1c0466a5267f178cdbea1a
StoneInHisWorld <1195290789@qq.com>
2024-06-26 19:44:22 +0800

    trying to preprocess dataset by multiprocessing but fail.

--- data_related/datasets.py
+++ data_related/datasets.py
@@ -1,3 +1,4 @@
+import time
 from typing import Iterable, Callable, List, Tuple
 
 import torch
@@ -5,11 +6,42 @@ from torch.utils.data import Dataset as torch_ds, DataLoader
 from tqdm import tqdm
 
 import utils.func.pytools as tools
+from multiprocessing import Queue, Event, Manager
+from multiprocessing import Process, Pipe
+import dill
+
+
+def fea_apply(result_pipe, features, features_calls, pbar_Q: Queue):
+    features_calls = dill.loads(features_calls)
+    n_tasks = len(features_calls)
+    for i, call in enumerate(features_calls):
+        features = call(features)
+        # shared_value['fea'] = call(shared_value['fea'])
+        pbar_Q.put(f'特征集预处理完成{i + 1}/{n_tasks}')
+        pbar_Q.put(1)
+    pbar_Q.put(True)
+    features = features.share_memory_()
+    result_pipe.send(features)
+
+
+def lb_apply(result_pipe, labels, labels_calls, pbar_Q: Queue):
+    # self = dill.loads(self)
+    labels_calls = dill.loads(labels_calls)
+    n_tasks = len(labels_calls)
+    for i, call in enumerate(labels_calls):
+        labels = call(labels)
+        # shared_value['lb'] = call(shared_value['lb'])
+        pbar_Q.put(f'标签集预处理完成{i + 1}/{n_tasks}')
+        pbar_Q.put(1)
+    pbar_Q.put(True)
+    labels = labels.share_memory_()
+    result_pipe.send(labels)
 
 
 class DataSet(torch_ds):
 
-    def __init__(self, features, labels, collate_fn: Callable = None):
+    def __init__(self, features, labels,
+                 collate_fn: Callable = None):
         """
         普通数据集，存储数据实际内容供DataLoader进行读取。
         :param features: 数据特征集
@@ -36,11 +68,12 @@ class DataSet(torch_ds):
         :param device: 迁移目标设备
         :return: None
         """
-        assert type(self._features) == torch.Tensor, f'数据集特征集数据未转换成张量，无法迁移到{device}中！请在数据读取步骤和预处理步骤将特征集转换成张量。'
-        assert type(self._features) == torch.Tensor, f'数据集标签集数据未转换成张量，无法迁移到{device}中！请在数据读取步骤和预处理步骤将标签集转换成张量。'
+        assert type(self._features) == torch.Tensor, \
+            f'数据集特征集数据未转换成张量，无法迁移到{device}中！请在数据读取步骤和预处理步骤将特征集转换成张量。'
+        assert type(self._features) == torch.Tensor, \
+            f'数据集标签集数据未转换成张量，无法迁移到{device}中！请在数据读取步骤和预处理步骤将标签集转换成张量。'
         self._features = self._features.to(device)
         self._labels = self._labels.to(device)
-        # self.collate_fn = lambda input:
 
     def apply(self, features_calls: List[Callable[[torch.Tensor], torch.Tensor]] = None,
               labels_calls: List[Callable[[torch.Tensor], torch.Tensor]] = None,
@@ -57,6 +90,7 @@ class DataSet(torch_ds):
             features_calls = []
         if labels_calls is None:
             labels_calls = []
+
         pbar = tqdm(total=len(features_calls) + len(labels_calls),
                     unit='步', position=0, desc=desc, mininterval=1)
 
@@ -75,11 +109,52 @@ class DataSet(torch_ds):
             (fea_apply, (), {}),
             (lb_apply, (), {})
         )
+
+        # TODO：尝试用多进程加速
+        # pbar_Q = Queue()
+        # fea_rec_conn, fea_send_conn = Pipe()
+        # lb_rec_conn, lb_send_conn = Pipe()
+        # fea_preprocessing = Process(
+        #     target=fea_apply,
+        #     args=(
+        #         fea_send_conn,
+        #         self._features, dill.dumps(features_calls), pbar_Q
+        #     )
+        # )
+        # lb_preprocessing = Process(
+        #     target=lb_apply,
+        #     args=(
+        #         lb_send_conn,
+        #         self._labels, dill.dumps(labels_calls), pbar_Q
+        #     )
+        # )
+        # del self._features, self._labels
+        # pbar = tqdm(total=len(features_calls) + len(labels_calls),
+        #             unit='步', position=0, desc=desc, mininterval=1)
+        # fea_preprocessing.start()
+        # lb_preprocessing.start()
+        # flag = 0
+        # while True:
+        #     item = pbar_Q.get(True)
+        #     if type(item) == int:
+        #         pbar.update(item)
+        #     elif type(item) == str:
+        #         pbar.set_description(item)
+        #     elif type(item) == bool:
+        #         flag += 1
+        #         if flag == 2:
+        #             pbar.set_description('预处理完成')
+        #             break
+        #     else:
+        #         raise ValueError(f'无法识别的信号{item}')
+        # # fea_preprocessing.join()
+        # # lb_preprocessing.join()
+        # self._features = fea_rec_conn.recv()
+        # self._labels = lb_rec_conn.recv()
+        # # self._labels = lb_preprocessing.get_result()
+        # fea_preprocessing.kill()
+        # lb_preprocessing.kill()
         pbar.close()
-        # for call in features_calls:
-        #     self._features = call(self._features)
-        # for call in labels_calls:
-        #     self._labels = call(self._labels)
 
     def to_loader(self, batch_size: int = None, shuffle=True,
                   sampler: Iterable = None, preprocess: bool = True,
@@ -223,4 +298,3 @@ class LazyDataSet(DataSet):
             self._labels = self._labels.to(device)
         elif to not in self.lb_preprocesses:
             self.lb_preprocesses += [to]
-
