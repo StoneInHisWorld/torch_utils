be6c4798628c9e0c6cced57f1726d7d3a7e1846c
StoneInHisWorld <1195290789@qq.com>
2024-07-29 20:56:09 +0800

    add handler for ConstantInputWarnings

--- data_related/criteria.py
+++ data_related/criteria.py
@@ -1,7 +1,11 @@
+import warnings
+
+import scipy.stats
 import torch
 from skimage.metrics import (structural_similarity as ssim,
                              peak_signal_noise_ratio as psnr)
 from scipy.stats import pearsonr
+from scipy.stats import ConstantInputWarning
 
 import networks.layers.pcc
 from data_related.ds_operation import normalize
@@ -35,15 +39,20 @@ def SSIM(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
 
 
 def PSNR(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
-    # y = Y.cpu().numpy().squeeze()
-    # y_hat = Y_HAT.cpu().numpy().squeeze()
     y = normalize(Y).cpu().numpy()
     y_hat = normalize(Y_HAT).cpu().numpy()
-    if len(y.shape) == 2 and len(y_hat.shape) == 2:
-        # 如果只有一张图片
-        result = torch.tensor(psnr(y, y_hat, data_range=1))
-    else:
-        result = torch.tensor([psnr(i, j, data_range=1) for i, j in zip(y, y_hat)])
+    with warnings.catch_warnings(record=True) as warning_filter:
+        warnings.simplefilter("ignore", ConstantInputWarning)
+        if len(y.shape) == 2 and len(y_hat.shape) == 2:
+            # 如果只有一张图片
+            result = torch.tensor(psnr(y, y_hat, data_range=1))
+        else:
+            result = torch.tensor([psnr(i, j, data_range=1) for i, j in zip(y, y_hat)])
+        if warning_filter:
+            for warning in warning_filter:
+                if issubclass(warning.category, ConstantInputWarning):
+                    print(f'计算皮尔逊相关系数时遇到常数输入，此时相关系数无定义！输入如下：\n'
+                          f'Y_HAT:{Y_HAT.item()}\nY:{Y.item()}')
     if size_averaged:
         return torch.sum(result)
     else:
