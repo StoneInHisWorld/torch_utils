b868d6ddc24d8f95fb747c369f0daf749e2319ce
StoneInHisWorld <1195290789@qq.com>
2025-04-04 12:17:07 +0800

    delete unused codes

--- data_related/criteria.py
+++ data_related/criteria.py
@@ -44,34 +44,8 @@ def SSIM(Y_HAT: torch.Tensor, Y: torch.Tensor, data_range=255., size_averaged: b
         kwargs = {'data_range': data_range, 'reduction': 'sum'}
     else:
         kwargs = {'data_range': data_range, 'reduction': 'none'}
-    #
-    # with warnings.catch_warnings(record=True) as warning_filter:
-    # warnings.simplefilter("ignore", UserWarning)
     computer = StructuralSimilarityIndexMeasure(**kwargs).to(Y_HAT.device)
     return computer(Y_HAT, Y)
-        # def msg_printer(*input_args):
-        #     constant_msg = ''
-        #     y, y_hat = input_args
-        #     constant_msg += f'Y:{y[0]}' if np.all(y == y[0]) else ''
-        #     constant_msg += f'Y_HAT:{y_hat[0]}' if np.all(y_hat == y_hat[0]) else ''
-        #     print(f'\n计算皮尔逊相关系数时遇到常数输入，此时相关系数无定义！常数输入如下：\n{constant_msg}')
-        #
-        # pearsonr_impl = functools.partial(
-        #     warning_handler, func=lambda y, y_hat: PearsonCorrCoef(y, y_hat)[0],
-        #     category=ConstantInputWarning, warning_filter=warning_filter, warning_msg_printer=msg_printer
-        # )
-
-    # y = Y.cpu().numpy().squeeze()
-    # y_hat = Y_HAT.cpu().numpy().squeeze()
-    # if len(y.shape) == 2 and len(y_hat.shape) == 2:
-    #     # 如果只有一张图片
-    #     result = torch.tensor(ssim(y, y_hat, channel_axis=0, data_range=1))
-    # else:
-    #     result = torch.tensor([ssim(i, j, channel_axis=0, data_range=1) for i, j in zip(y, y_hat)])
-    # if size_averaged:
-    #     return torch.sum(result)
-    # else:
-    #     return result
 
 
 def PSNR(Y_HAT: torch.Tensor, Y: torch.Tensor, data_range=255., size_averaged: bool = True):
@@ -92,22 +66,8 @@ def PSNR(Y_HAT: torch.Tensor, Y: torch.Tensor, data_range=255., size_averaged: b
         kwargs = {'data_range': data_range, 'reduction': 'sum', 'dim': list(range(1, len(Y_HAT.shape)))}
     else:
         kwargs = {'data_range': data_range, 'reduction': 'none', 'dim': list(range(1, len(Y_HAT.shape)))}
-    # with warnings.catch_warnings(record=True) as warning_filter:
-    #     warnings.simplefilter("ignore", UserWarning)
     computer = PeakSignalNoiseRatio(**kwargs).to(Y_HAT.device)
     return computer(Y_HAT, Y)
-
-    # y = normalize(Y).cpu().numpy()
-    # y_hat = normalize(Y_HAT).cpu().numpy()
-    # if len(y.shape) == 2 and len(y_hat.shape) == 2:
-    #     # 如果只有一张图片
-    #     result = torch.tensor(psnr(y, y_hat, data_range=1))
-    # else:
-    #     result = torch.tensor([psnr(i, j, data_range=1) for i, j in zip(y, y_hat)])
-    # if size_averaged:
-    #     return torch.sum(result)
-    # else:
-    #     return result
 
 
 def PCC(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
@@ -127,54 +87,3 @@ def PCC(Y_HAT: torch.Tensor, Y: torch.Tensor, size_averaged: bool = True):
         kwargs = {'reduction': 'none'}
     computer = PearsonCorrCoef(**kwargs)
     return computer(Y_HAT, Y)
-#     with warnings.catch_warnings(record=True) as warning_filter:
-#         warnings.simplefilter("default", ConstantInputWarning)
-#
-#         def msg_printer(*input_args):
-#             constant_msg = ''
-#             y, y_hat = input_args
-#             constant_msg += f'Y:{y[0]}' if np.all(y == y[0]) else ''
-#             constant_msg += f'Y_HAT:{y_hat[0]}' if np.all(y_hat == y_hat[0]) else ''
-#             print(f'\n计算皮尔逊相关系数时遇到常数输入，此时相关系数无定义！常数输入如下：\n{constant_msg}')
-#
-#         pearsonr_impl = functools.partial(
-#             warning_handler, func=lambda y, y_hat: PearsonCorrCoef(y, y_hat)[0],
-#             category=ConstantInputWarning, warning_filter=warning_filter, warning_msg_printer=msg_printer
-#         )
-#         if len(y.shape) == 2 and len(y_hat.shape) == 2:
-#             # 如果只有一张图片
-#             result = torch.tensor(pearsonr_impl(y.flatten(), y_hat.flatten()))
-#         else:
-#             result = torch.tensor([pearsonr_impl(i.flatten(), j.flatten()) for i, j in zip(y, y_hat)])
-#     if size_averaged:
-#         return torch.sum(result)
-#     else:
-#         return result
-#     computer = PearsonCorrCoef(**kwargs)
-#     return computer(Y_HAT, Y)
-
-    # y = normalize(Y).cpu().numpy()
-    # y_hat = normalize(Y_HAT).cpu().numpy()
-    # with warnings.catch_warnings(record=True) as warning_filter:
-    #     warnings.simplefilter("default", ConstantInputWarning)
-    #
-    #     def msg_printer(*input_args):
-    #         constant_msg = ''
-    #         y, y_hat = input_args
-    #         constant_msg += f'Y:{y[0]}' if np.all(y == y[0]) else ''
-    #         constant_msg += f'Y_HAT:{y_hat[0]}' if np.all(y_hat == y_hat[0]) else ''
-    #         print(f'\n计算皮尔逊相关系数时遇到常数输入，此时相关系数无定义！常数输入如下：\n{constant_msg}')
-    #
-    #     pearsonr_impl = functools.partial(
-    #         warning_handler, func=lambda y, y_hat: pearsonr(y, y_hat)[0],
-    #         category=ConstantInputWarning, warning_filter=warning_filter, warning_msg_printer=msg_printer
-    #     )
-    #     if len(y.shape) == 2 and len(y_hat.shape) == 2:
-    #         # 如果只有一张图片
-    #         result = torch.tensor(pearsonr_impl(y.flatten(), y_hat.flatten()))
-    #     else:
-    #         result = torch.tensor([pearsonr_impl(i.flatten(), j.flatten()) for i, j in zip(y, y_hat)])
-    # if size_averaged:
-    #     return torch.sum(result)
-    # else:
-    #     return result
--- layers/pcc.py
+++ layers/pcc.py
@@ -14,22 +14,6 @@ def calculate_pcc(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     :return: 两张图片的PCC值
     """
     assert y_hat.shape == y.shape, '计算PCC的两个张量形状需一致'
-    # shape_dim = list(range(2, len(y.shape)))
-    # mean_x, mean_y = [t.mean(dim=shape_dim, keepdim=True) for t in [y_hat, y]]
-    # std_x, std_y = [t.std(dim=shape_dim) for t in [y_hat, y]]
-    # conv_xy = ((y_hat - mean_x) * (y - mean_y)).sum(dim=list(range(2, len(y.shape)))) / (
-    #         y.shape[-1] * y.shape[-2] - 1
-    # )
-    # 计算分子
-    # numerator = ((y_hat - mean_x) * (y - mean_y)).sum(dim=shape_dim)
-    # 计算分母
-    # x_coefficient = (y_hat.shape[-1] * y_hat.shape[-2] - 1) ** 0.5
-    # y_coefficient = (y.shape[-1] * y.shape[-2] - 1) ** 0.5
-    # denominator = std_x * x_coefficient
-    # denominator *= std_y * y_coefficient
-    # denominator = std_x * std_y
-    # return conv_xy / denominator.squeeze() + epsilon
-
     # 新方法计算PCC
     corrcoef_xy = [
         torch.corrcoef(o)[0, 1]
@@ -75,7 +59,3 @@ class PCC(nn.Module):
             return result.sum()
         else:
             return result
-        # if self.size_averaged:
-        #     return result.mean()
-        # else:
-        #     return result.squeeze()
