9826527aca5228c347e441af21d8623d347cc094
StoneInHisWorld <1195290789@qq.com>
2025-04-09 21:44:21 +0800

    new version of settings.json

--- config/init_cfg.py
+++ config/init_cfg.py
@@ -17,33 +17,73 @@ def init_settings(path):
 
     :param path: 配置文件存储路径
     """
+    # default_settings = {
+    #     "data_portion": 0.1,
+    #     "train_portion": 0.8,
+    #     "random_seed": 42,
+    #     "plot_mute": True,
+    #     "plot_history": "save",
+    #     "print_net": False,
+    #     "save_net": "state",
+    #     "device": "cpu",
+    #     "lazy": False,
+    #     "shuffle": True,
+    #     "n_workers": 4,
+    #     "pin_memory": False,
+    #     "cuda_memrecord": False,
+    #     "prefetch_factor": 2,
+    #     "max_prefetch": 2,
+    #     "share_memory": False,
+    #     "bkg_gen": True,
+    #     "with_hook": False,
+    #     "hook_mute": True,
+    #     "bulk_preprocess": True,
+    #     "with_checkpoint": False,
+    #     "f_req_sha": (256, 256),
+    #     "l_req_sha": (256, 256),
+    #     "which_dataset": "which dataset?",
+    #     "log_root": "../../log",
+    #     "dataset_root": "where you put your datasets"
+    # }
+
     default_settings = {
-        "data_portion": 0.1,
-        "train_portion": 0.8,
         "random_seed": 42,
         "plot_mute": True,
         "plot_history": "save",
-        "print_net": False,
         "save_net": "state",
         "device": "cpu",
-        "lazy": False,
-        "shuffle": True,
-        "n_workers": 4,
-        "pin_memory": False,
+        "n_workers": 8,
         "cuda_memrecord": False,
-        "prefetch_factor": 2,
-        "max_prefetch": 2,
-        "share_memory": False,
-        "bkg_gen": True,
-        "with_hook": False,
-        "hook_mute": True,
-        "bulk_preprocess": True,
-        "with_checkpoint": False,
-        "f_req_sha": (256, 256),
-        "l_req_sha": (256, 256),
-        "which_dataset": "which dataset?",
         "log_root": "../../log",
-        "dataset_root": "where you put your datasets"
+        "t_kwargs": {
+            "with_hook": False,
+            "hook_mute": True,
+            "with_checkpoint": False,
+            "print_net": False,
+            "device": {"$ref": "#/device"},
+            "n_workers": {"$ref": "#/n_workers"}
+        },
+        "ds_kwargs": {
+            "dataset_root": "../../datasets",
+            "n_workers": {"$ref": "#/n_workers"},
+            "data_portion": 0.01,
+            "bulk_preprocess": False,
+            "shuffle": True,
+            "which_dataset": "which?",
+            "f_req_sha": [128, 128],
+            "l_req_sha": [128, 128],
+            "lazy": False
+        },
+        "dl_kwargs": {
+            "train_portion": 0.8, "max_prefetch": 2, "pin_memory": True,
+            "prefetch_factor": None, "bkg_gen": True,
+            "num_workers": {"$ref": "#/n_workers"},
+            "transit_kwargs": {
+                "device": {"$ref": "#/device"},
+                "share_memory": False,
+                "non_blocking": True
+            }
+        }
     }
 
     with open(path, 'w', encoding='utf-8') as f:
