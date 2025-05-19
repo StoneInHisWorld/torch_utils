65e687c948c0232cd0364a5830a5f3d92e65a8df
StoneInHisWorld <1195290789@qq.com>
2025-05-19 10:38:39 +0800

    update resnet50 and Patchgenerator

--- networks/nets/__init__.py
+++ networks/nets/__init__.py
@@ -9,3 +9,5 @@ from .slp import SLP
 from .vgg import VGG
 from .wzynet_essay import WZYNetEssay
 from .elnn.enlargingnet import EnLargingNN
+from .patches_generator import PatchGenerator
+from .resnet import ResNet50
