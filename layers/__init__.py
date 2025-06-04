5fdbccbf72497a47318d351da7b59fa304fda4b9
StoneInHisWorld <1195290789@qq.com>
2025-06-04 14:23:23 +0800

    add kvcache transformer and auto_regression algorithm.

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,3 +1,5 @@
 from .pcc import PCC, PCCLoss
 from .ganloss import GANLoss
 from .ssim import SSIMLoss
+from .kvcache_Transformer import KVCacheTransformerBlock, KVCache_TransformerEncoderLayer, KVCache_TransformerDecoderLayer
+from .kvcache_Transformer import auto_regression
