da519c4acc1c4d8fd714889a541eeea64fcf92bf
StoneInHisWorld <1195290789@qq.com>
2025-07-21 21:09:29 +0800

    delete unused codes

--- networks/nets/itransformer.py
+++ networks/nets/itransformer.py
@@ -11,50 +11,8 @@ from layers.add_positionEmbeddings import AddPositionEmbs
 from layers import KVCacheTransformerBlock, auto_regression
 
 
-#
-#
-# class PatchTransformerDecoder(BasicNN):
-#     def __init__(self, vocab_size, key_size, query_size, value_size,
-#                  num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
-#                  num_heads, num_layers, dropout, **kwargs):
-#         self.num_hiddens = num_hiddens
-#         self.num_layers = num_layers
-#         self.embedding = nn.Embedding(vocab_size, num_hiddens)
-#         self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
-#         self.blks = nn.Sequential()
-#         blk_s = [
-#             DecoderBlock(key_size, query_size, value_size, num_hiddens,
-#                          norm_shape, ffn_num_input, ffn_num_hiddens,
-#                          num_heads, dropout, i)
-#             for i in range(num_layers)
-#         ]
-#         self.dense = nn.Linear(num_hiddens, vocab_size)
-#         super(PatchTransformerDecoder, self).__init__(**kwargs)
-#
-#     def init_state(self, enc_outputs, enc_valid_lens, *args):
-#         return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
-#
-#     def forward(self, X, state):
-#         X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
-#         self._attention_weights = [[None] * len(self.blks) for _ in range (2)]
-#         for i, blk in enumerate(self.blks):
-#             X, state = blk(X, state)
-#             # 解码器自注意力权重
-#             self._attention_weights[0][
-#                 i] = blk.attention1.attention.attention_weights
-#             # “编码器－解码器”自注意力权重
-#             self._attention_weights[1][
-#                 i] = blk.attention2.attention.attention_weights
-#         return self.dense(X), state
-#
-#     @property
-#     def attention_weights(self):
-#         return self._attention_weights
-#
-
 def SEQ_ENTROLOSS(pred, y, unwrapped_entroloss):
     pred = pred.permute(0, 2, 1)
-    # y = y.argmax(2)
     return unwrapped_entroloss(pred, y)
 
 
@@ -92,7 +50,6 @@ class ITransformer(BasicNN):
         else:
             pos_embedding = nn.Identity()
         transformer = KVCacheTransformerBlock(in_len, out_len, d_model, auto_regressive=auto_regression, **tran_kwargs)
-        # transformer = nn.Transformer(d_model, batch_first=True, **tran_kwargs)
         linear_projector = nn.Linear(d_model, out_pixel_range)
         self.pixel_basis = torch.arange(0, out_pixel_range).reshape(1, out_pixel_range, 1).to(torch.float32)
         # self.patches_size = patches_size
@@ -114,60 +71,6 @@ class ITransformer(BasicNN):
         Returns:
 
         """
-        # inputs, labels_or_shape = inputs
-        # lb_mask = nn.Transformer.generate_square_subsequent_mask(labels_or_shape.size()[-1])
-        # lb_mask = (lb_mask == -torch.inf).to(inputs.device)
-        # inputs = self.pos_embedding(self.f_embedding(inputs))
-        # labels_or_shape = self.pos_embedding(self.l_embedding(labels_or_shape))
-        # num_pixel_in_patch = self.patches_size ** 2
-        # for i, l_slice in enumerate(torch.split(labels_or_shape, num_pixel_in_patch, 1)):
-        #     i = i * num_pixel_in_patch
-        #     l_mask = lb_mask[i: i + num_pixel_in_patch, i: i + num_pixel_in_patch]
-        #     # 图片处理不含padding_mask
-        #     outputs.append(self.transformer(inputs, l_slice, tgt_mask=l_mask))
-        # outputs = torch.cat(outputs, 1)
-        # outputs = self.transformer(inputs, labels_or_shape, tgt_mask=lb_mask)
-        # if torch.is_grad_enabled():
-        #     outputs = self.linear_projector(outputs)
-        #     return F.softmax(outputs, 2)
-        # else:
-        #     # 预测结果只需要看最后一个词
-        #     outputs = self.linear_projector(outputs[:, -1])
-        #     return F.softmax(outputs, 1)
-        # outputs = torch.cat(outputs, 1)
-        # return self.linear_projector(outputs)
-        # if torch.is_grad_enabled():
-        #     # 强制教学
-        #     # i_bs, i_len, i_c = inputs.shape
-        #     # l_bs, l_len, l_c = labels_or_shape.shape
-        #     # # 将序列长度移动到第1维，通道维移动到第2维
-        #     # inputs = inputs.permute(0, 2, 3, 1)
-        #     # labels_or_shape = labels_or_shape.permute(0, 2, 3, 1)
-        #     # inputs = inputs.reshape([i_bs, i_h * i_w, i_c])
-        #     # labels_or_shape = labels_or_shape.reshape([l_bs, l_h * l_w, l_c])
-        #     # 使用F.pad填充
-        #     # f_kwargs = {}
-        #     # if i_c > l_c:
-        #     #     labels_or_shape = F.pad(labels_or_shape, (0, i_c - l_c, 0, 0), mode='constant', value=0)
-        #     # elif i_c < l_c:
-        #     #     inputs = F.pad(inputs, (0,  l_c - i_c, 0, 0), mode='constant', value=0)
-        #     for l_slice in torch.split(labels_or_shape, self.patches_size ** 2, 1):
-        #         l_mask = nn.Transformer.generate_square_subsequent_mask(labels_or_shape.size()[-1])
-        #         # 图片处理不含padding_mask
-        #         outputs.append(self.transformer(inputs, l_slice))
-        #     outputs = torch.cat(outputs, 1)
-        #     return self.linear_projector(outputs)
-        # else:
-        #     # 预测
-        #     # inputs = self.position_embedding(inputs)
-        #     bs, seq_len, seq_dim = labels_or_shape
-        #     for _ in range(seq_len):
-        #         memory = self.transformer(
-        #             torch.zeros(bs, self.patches_size ** 2, seq_dim),
-        #             inputs
-        #         )
-        #         outputs.append(memory[:, -1].unsqueeze(1))
-        # self.transformer.refresh_head(inputs.shape[0], inputs.device)
         if self.auto_reg and not torch.is_grad_enabled():
             inputs = self.pos_embedding(self.f_embedding(inputs))
             b, _, d = inputs.shape
@@ -194,31 +97,9 @@ class ITransformer(BasicNN):
         :param y: 标签集
         :return: （预测值， （损失值集合））
         """
-        # 填充至词维度一致
-        # embd_size = X.shape[2] - y.shape[2]
-        # if embd_size < 0:
-        #     X = torch.nn.functional.pad(
-        #         X, (0, -embd_size, 0, 0), mode='constant', value=0
-        #     )
-        # elif embd_size > 0:
-        #     y = torch.nn.functional.pad(
-        #         y, (0, embd_size, 0, 0), mode='constant', value=0
-        #     )
-        # 前向传播
-        # if torch.is_grad_enabled():
-        #     pred = self((X, y))
-        #     ls_es = [ls_fn(pred, y) for ls_fn in self.train_ls_fn_s]
-        #     # pred = torch.matmul(pred, self.pixel_basis).squeeze()
-        #     # return pred, ls_es
-        # else:
-        #     # 自回归预测
-        #     pred = self.__auto_regressive_predict(X, y.shape)
-        #     ls_es = [ls_fn(pred, y) for ls_fn in self.test_ls_fn_s]
         if torch.is_grad_enabled():
             pred = self((X, y))
             ls_es = [ls_fn(pred, y) for ls_fn in self.train_ls_fn_s]
-            # pred = torch.matmul(pred, self.pixel_basis).squeeze()
-            # return pred, ls_es
         else:
             # 自回归预测
             pred = self(X)
@@ -226,23 +107,6 @@ class ITransformer(BasicNN):
         self.pixel_basis = self.pixel_basis.to(X.device)
         pred = (pred @ self.pixel_basis).squeeze()
         return pred, ls_es
-        # pred = self((X, y.shape))
-        # 去掉标签集中填充部分以计算损失值
-        # if embd_size > 0:
-        #     y = y[:, :, :-embd_size]
-
-    # def __auto_regressive_predict(self, X, label_shape):
-    #     """自回归预测
-    #     参考自：https://www.zhihu.com/question/584772471/answer/2900724691
-    #     """
-    #     # 初始化预测向量
-    #     bs, seq_len = label_shape
-    #     targets = torch.zeros(bs, 1, self.pixel_basis.shape[-2]).to(torch.int).to(X.device)
-    #     for _ in range(seq_len):
-    #         output = self((X, targets.argmax(2)))
-    #         output = F.softmax(output.unsqueeze(1), 2)
-    #         targets = torch.cat([targets, output], 1)
-    #     return targets[:, 1:].to(torch.float32)
 
     def _get_ls_fn(self, *args):
         train_ls_fn_s, train_ls_names, test_ls_fn_s, test_ls_names = super()._get_ls_fn(*args)
