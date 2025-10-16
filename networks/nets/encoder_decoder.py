acfa0779b19dae397b8ef5406baf0cd654f3a26c
StoneInHisWorld <1195290789@qq.com>
2025-10-16 16:42:39 +0800

    add encoder decoder structure

---
+++ networks/nets/encoder_decoder.py
@@ -0,0 +1,35 @@
+from abc import abstractmethod
+from collections import OrderedDict
+
+from .. import BasicNN
+
+
+class Encoder(BasicNN):
+
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("Encoder解码器类是抽象类，需要继承之后才能使用")
+
+
+class Decoder(BasicNN):
+
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("Decoder解码器类是抽象类，需要继承之后才能使用")
+
+    @abstractmethod
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError()
+
+class EncoderDecoder(BasicNN):
+
+    def __init__(self, encoder, decoder, *args, **kwargs):
+        super(EncoderDecoder, self).__init__(*args, OrderedDict([
+            ('encoder', encoder), ('decoder', decoder)
+        ]), **kwargs)
+
+    def forward(self, input):
+        enc_X, dec_X = input
+        enc_outputs = self.encoder(enc_X)
+        dec_state = self.decoder.init_state(enc_outputs)
+        return self.decoder((dec_X, dec_state))
